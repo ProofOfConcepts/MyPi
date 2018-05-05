247ed1938ff878662807586b7a59d9806078c60a
Pradeep Singh <pradeep.ansh.sumit@gmail.com>
2018-05-06 01:09:40 +0700

    added startup sound playback feature

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+#!usr/bin/env python  
+#coding=utf-8  
+
 from __future__ import print_function
 
 import logging
 import argparse
 import os.path
 import json
+import pyaudio  
+import wave  
+import sys
+import os 
 
 import google.auth.transport.requests
 import google.oauth2.credentials
@@ -15,9 +22,52 @@ from google.assistant.library.file_helpers import existing_file
 
 DEVICE_API_URL = 'https://embeddedassistant.googleapis.com/v1alpha2'
 
+CHUNK_SIZE = 1024
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 logging.basicConfig(filename='/tmp/MyPi.log', level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger=logging.getLogger(__name__)
+
+def play_wav(wav_filename, chunk_size=CHUNK_SIZE):
+    '''
+    Play (on the attached system sound device) the WAV file
+    named wav_filename.
+    '''
+
+    try:
+        print('Welcome...\nNow starting the assistant')
+        wf = wave.open(wav_filename, 'rb')
+    except IOError as ioe:
+        sys.stderr.write('IOError on file ' + wav_filename + '\n' + \
+        str(ioe) + '. Skipping.\n')
+        return
+    except EOFError as eofe:
+        sys.stderr.write('EOFError on file ' + wav_filename + '\n' + \
+        str(eofe) + '. Skipping.\n')
+        return
+
+    # Instantiate PyAudio.
+    p = pyaudio.PyAudio()
+
+    # Open stream.
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+        channels=wf.getnchannels(),
+        rate=wf.getframerate(),
+                    output=True)
+
+    data = wf.readframes(chunk_size)
+    while len(data) > 0:
+        stream.write(data)
+        data = wf.readframes(chunk_size)
+
+    # Stop stream.
+    stream.stop_stream()
+    stream.close()
+
+    # Close PyAudio.
+    p.terminate()
 
 def process_device_actions(event, device_id):
     if 'inputs' in event.args:
@@ -121,6 +171,8 @@ def main():
                                                             **json.load(f))
 
     with Assistant(credentials, args.device_model_id) as assistant:
+        # Play intro audio
+        play_wav(dir_path + "/assets/startup.wav")
         events = assistant.start()
 
         print('device_model_id:', args.device_model_id + '\n' +
