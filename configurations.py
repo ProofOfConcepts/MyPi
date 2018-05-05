603fd925b1064797788dc9764951158a444a9d2b
Pradeep Singh <pradeep.ansh.sumit@gmail.com>
2018-05-06 01:59:53 +0700

    added config file

--- configurations.py
+++ configurations.py
@@ -3,7 +3,7 @@ import os
 def getwelcomemessage():
     file_path = os.path.dirname(os.path.realpath(__file__)) + "/config.json"
     print("trying to open file: " + file_path)
-    with open(file_path, encoding='utf-8') as file:
+    with open(file_path) as file:
         try:
             data = json.load(file)
             message = data["startupmessage"]
@@ -11,3 +11,9 @@ def getwelcomemessage():
         except Exception as error:
             print("There was an error reading configuration file\n" + error)
             return "Welcome, If you need me just say Hello Google or Hey Google"
+
+def main():
+    print(getwelcomemessage())
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import logging
 import argparse
 import os.path
 import json
-import pyaudio  
-import wave  
 import sys
 import os 
 import subprocess
@@ -26,54 +24,13 @@ from configurations import getwelcomemessage
 
 DEVICE_API_URL = 'https://embeddedassistant.googleapis.com/v1alpha2'
 
-CHUNK_SIZE = 1024
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 logging.basicConfig(filename='/tmp/MyPi.log', level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger=logging.getLogger(__name__)
 
-welcomemessage = getwelcomemessage
-
-def play_wav(wav_filename, chunk_size=CHUNK_SIZE):
-    '''
-    Play (on the attached system sound device) the WAV file
-    named wav_filename.
-    '''
-
-    try:
-        print('Welcome...\nNow starting the assistant')
-        wf = wave.open(wav_filename, 'rb')
-    except IOError as ioe:
-        sys.stderr.write('IOError on file ' + wav_filename + '\n' + \
-        str(ioe) + '. Skipping.\n')
-        return
-    except EOFError as eofe:
-        sys.stderr.write('EOFError on file ' + wav_filename + '\n' + \
-        str(eofe) + '. Skipping.\n')
-        return
-
-    # Instantiate PyAudio.
-    p = pyaudio.PyAudio()
-
-    # Open stream.
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-        channels=wf.getnchannels(),
-        rate=wf.getframerate(),
-                    output=True)
-
-    data = wf.readframes(chunk_size)
-    while len(data) > 0:
-        stream.write(data)
-        data = wf.readframes(chunk_size)
-
-    # Stop stream.
-    stream.stop_stream()
-    stream.close()
-
-    # Close PyAudio.
-    p.terminate()
+welcomemessage = getwelcomemessage()
 
 def process_device_actions(event, device_id):
     if 'inputs' in event.args:
