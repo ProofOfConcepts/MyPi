8897bd307e5182ff8e7849c6963a4914c5f74cc6
Pradeep Singh <pradeep.ansh.sumit@gmail.com>
2018-05-06 07:16:39 +0700

    added the test action package for jukebox

--- main.py
+++ main.py
@@ -134,7 +134,8 @@ def main():
         #indicate that we are ready now...
         playWavFile(dir_path + configData[constants.HOTWORD_WAITING_AUDIO_FILE_KEY])
         for event in events:
-            print("processing event: " + str(event))
+            print("processing event: " + str(event) + "\nargs: " + str(event.args))
+
             process_event(event, assistant.device_id)
 
 if __name__ == '__main__':
