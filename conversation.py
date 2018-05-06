3fbca34b2ba5c6d93694e283494e8cb2c5be3280
Pradeep Singh <pradeep.ansh.sumit@gmail.com>
2018-05-06 12:31:24 +0700

    added the test action package for jukebox

--- conversation.py
+++ conversation.py
@@ -2,7 +2,6 @@
 #coding=utf-8  
 
 from gtts import gTTS
-from google.assistant.library import Assistant
 from googletrans import Translator
 import os
 import subprocess
@@ -20,10 +19,6 @@ def say(words, ttsfilename):
     tts.save(ttsfilename)
     playMp3File(ttsfilename)
     os.remove(ttsfilename)
-
-def sayAssistant(assistant, text):
-    text_to_speech = '<speak>' + text + '<audio src="https://actions.google.com/sounds/v1/alarms/digital_watch_alarm_long.ogg">a digital watch alarm</audio>.</speak>'
-    assistant.tell(text_to_speech)
 
 def playWavFile(fileName):
     subprocess.Popen(["aplay", fileName], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
